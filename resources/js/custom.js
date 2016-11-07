644ebbafaa24f2a8b651ea93138dc96a3eedb52e
User <User>
2016-11-07 16:14:55 +0800

    styles

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,3 +1,10 @@
+// nicescroll
+$(document).ready(
+    function() { 
+        $("html").niceScroll();
+    }
+);
+
 // --FitText--
 $("#fittext1").fitText();
 $("#fittext2").fitText(1.2);
@@ -37,6 +44,39 @@ $('.responsive').slick({
   	]
 });
 
+$('.responsive-v').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+    ]
+});
+
 // stickHeader
 $(window).scroll(function() {
     if ($(this).scrollTop() > 1){  
@@ -45,4 +85,9 @@ $(window).scroll(function() {
     else{
         $('header').removeClass("sticky");
     }
+});
+
+// fadeOut when scroll
+$(window).scroll(function(){
+    $("#h-home").css("opacity", 1 - $(window).scrollTop() / 965);
 });
