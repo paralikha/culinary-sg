ddf81139cb6b802745139db1381da6c5d448a6db
User <User>
2016-11-08 15:26:24 +0800

    customScrollbar

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -5,11 +5,6 @@ $(document).ready(
     }
 );
 
-// --FitText--
-$("#fittext1").fitText();
-$("#fittext2").fitText(1.2);
-$("#fittext3").fitText(1.1, { minFontSize: '50px', maxFontSize: '75px' });
-
 //slickSlider
 $('.responsive').slick({
   	dots: false,
