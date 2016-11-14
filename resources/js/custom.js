f0719b9942e4f3565ab7b163edb786b5164f03d2
User <User>
2016-11-14 09:33:04 +0800

    lightbox

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,6 +1,5 @@
 //fullpage
 (function($) {
-  
     'use strict';
 
     var $isAnimatedSecond = $('.second .is-animated'),
@@ -17,20 +16,28 @@
         scrollOverflowOptions: {
             click: true
         },
+        // animate
         onLeave: function(index, nextIndex, direction) {
-            if( ( index == 1 || index == 2 ) && nextIndex == 3 ) { 
-                $isAnimatedSecond.addClass('animated bounceIn'); 
+            if (( index == 1 || index == 2 ) && nextIndex == 3 ) { 
+                $isAnimatedSecond.addClass('animated fadeIn'); 
                 $isAnimatedSecond.eq(0).css('animation-delay', '.3s');
                 $isAnimatedSecond.eq(1).css('animation-delay', '.4s');
                 $isAnimatedSecond.eq(2).css('animation-delay', '.5s');
                 $isAnimatedSecond.eq(3).css('animation-delay', '.6s');
                 $isAnimatedSecond.eq(4).css('animation-delay', '.7s');
                 $isAnimatedSecond.eq(5).css('animation-delay', '.8s');
-                $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
+                // $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
+            }
+
+            else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
+                $isAnimatedThird.eq(0).addClass('animated fadeInRightBig').css('animation-delay', '.3s'); 
+                $isAnimatedThird.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
+                $isAnimatedThirdSingle.addClass('animated bounceInDown').css('animation-delay', '1.2s');
             }
         }
     });
 })(jQuery);
+
 
 
 // lightSlider
